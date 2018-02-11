b3e513efcf064ae4c0de430af210500cd7040614
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-02-11 14:24:30 +0900

    B031

--- cpp/Bprob/028.cpp
+++ cpp/Bprob/028.cpp
@@ -11,12 +11,39 @@ using namespace std;
 
 int main()
 {
-  // input
-
+  // input & pre
+  string s;
+  cin >> s;
+  std::vector<int> v(6,0);
+  // int result = 0;
 
   // process
-
+  for(int i = 0; i < s.size(); i++ )
+  {
+    if (s[i] == 'A') {
+      v[0]++;
+    }
+    else if (s[i] == 'B') {
+      v[1]++;
+    }
+    else if (s[i] == 'C') {
+      v[2]++;
+    }
+    else if (s[i] == 'D') {
+      v[3]++;
+    }
+    else if (s[i] == 'E') {
+      v[4]++;
+    }
+    else if (s[i] == 'F') {
+      v[5]++;
+    }
+  }
 
   //output
-
+  for(int i = 0;i < 5; i++)
+  {
+    std::cout << v[i] << ' ';
+  }
+  std::cout << v[5] << endl; 
 }
