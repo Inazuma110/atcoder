1d628bd3b23c9804b93c1ff8ab2ae261382f3365
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-02-13 11:00:05 +0900

    C015.cpp

--- cpp/Cprob/015.cpp
+++ cpp/Cprob/015.cpp
@@ -11,20 +11,23 @@ using namespace std;
 
 int n , k;
 
-bool dfs(int num,int val){
+bool dfs(int num,int val,vector<vector<int>> vv){
+  if (num == n && val == 0) {
+      std::cout << "Found" << '\n';
+      exit(0);
+  }
   for(int i = 0; i < k; i++)
   {
-    if(num != n) dfs(num+1,val^vv[i]);
-    if (num == n && val == 0) return true;
+    if(num != n) dfs(num+1,val^vv[num][i],vv);
   }
-
+  return false;
 }
 
 int main()
 {
   // input & declare
-  int result = 0;
   cin >> n >> k;
+  int result = 0;
   int tmp;
   std::vector<vector<int>> vv(n,vector<int>(k,0));
   for(int i = 0; i < n; i++)
@@ -36,6 +39,9 @@ int main()
   }
 
   // process
+if (dfs(0,0,vv) == 0) {
+  cout << "Nothing" << endl;
+}
 
 
   //output
