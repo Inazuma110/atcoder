d0a2f559daea1f6b5e78af795c11dbdf3d766fcd
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-07-23 17:10:31 +0900

    fix: 048

--- cpp/newABC/048/main.cpp
+++ cpp/newABC/048/main.cpp
@@ -17,8 +17,10 @@ int main()
   ll res = 0;
   ll tmp1 = 0;
   ll tmp2 = 0;
-  tmp1 = (a-1) / c;
-  tmp2 = b / c;
+  tmp1 = (a-1) / c + 1;
+  tmp2 = b / c + 1;
+  if(a == 0) tmp1 = 0;
+  if(b == 0)tmp2 = 1;
   res = tmp2 - tmp1;
   cout << res << endl;
 }
