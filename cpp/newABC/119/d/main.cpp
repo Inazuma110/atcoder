12dfff5461ba02a8d67c41fb119647e11f638254
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-02-25 23:01:54 +0900

    abc119c

--- cpp/newABC/119/d/main.cpp
+++ cpp/newABC/119/d/main.cpp
@@ -10,83 +10,67 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> p;
+ll n, a, b, c;
+vector<ll> v;
+ll res = INT_MAX;
+
+void v_init(){
+  for (ll i = 0; i < n; i++) {
+    ll tmp;
+    cin >> tmp;
+    v.push_back(tmp);
+  }
+}
+
+void calc(vector<int> tmp){
+  int tmpa = 0;
+  int tmpb = 0;
+  int tmpc = 0;
+  ll mp = 0;
+  for(int i = 0; i < int(tmp.size()); i++){
+    if(tmp[i] == 1) {
+      if(tmpa != 0) mp += 10;
+      tmpa += v[i];
+    }
+    if(tmp[i] == 2) {
+      if(tmpb != 0) mp += 10;
+      tmpb += v[i];
+    }
+    if(tmp[i] == 3) {
+      if(tmpc != 0) mp += 10;
+      tmpc += v[i];
+    }
+  }
+  if(tmpa == 0 || tmpb == 0 || tmpc == 0) return;
+  mp += abs(tmpa - a);
+  mp += abs(tmpb - b);
+  mp += abs(tmpc - c);
+
+  res = min(res, mp);
+}
+
+void dfs(int which, vector<int> tmp, int step){
+  tmp.push_back(which);
+  if(step == n-1) {
+    calc(tmp);
+    return;
+  }
+  dfs(0, tmp, step+1);
+  dfs(1, tmp, step+1);
+  dfs(2, tmp, step+1);
+  dfs(3, tmp, step+1);
+
+}
+
 
 int main(){
-  ll n, a, b, c;
   cin >> n >> a >> b >> c;
-  vector<ll> v(n);
-  for (ll i = 0; i < n; i++) {
-    cin >> v[i];
+  v_init();
+  vector<int> tmp;
+  for (int i = 0; i < 4; i++) {
+    dfs(i, tmp,  0);
   }
 
-  // sort(all(v));
-  ll near_c = INT_MAX, near_b = INT_MAX, near_a = INT_MAX;
-  ll near_c_index = -1, near_b_index = -1, near_a_index = -1;
-
-  ll res = 0;
-  for (ll i = 0; i < n; i++) {
-    if(v[i] <= c && c - v[i] < abs(c - near_c)){
-      near_c = v[i];
-      near_c_index = i;
-    }
-  }
-  for (ll i = 0; i < n; i++) {
-    if(v[i] <= b && b - v[i] < abs(b - near_b) && i != near_c_index){
-      near_b = v[i];
-      near_b_index = i;
-    }
-  }
-  for (ll i = 0; i < n; i++) {
-    if(v[i] <= a && a - v[i] < abs(a - near_a) && i != near_c_index && i != near_b_index){
-      near_a = v[i];
-      near_a_index = i;
-    }
-  }
-
-  set<ll> use_index;
-
-  for (ll i = 0; i < n; i++) {
-    if(i == near_c_index || near_b_index == i ||  i == near_a_index)
-      continue;
-    if(use_index.count(i) == 1) continue;
-    if(abs(c - near_c) <= 10) break;
-    if(c-near_c > abs(c-(near_c+v[i]))+10 && v[i] > 10)
-    {
-      res += 10;
-      near_c += v[i];
-      use_index.insert(i);
-    }
-  }
-  res += abs(c - near_c);
-
-  for (ll i = 0; i < n; i++) {
-    if(i == near_c_index || near_b_index == i ||  i == near_a_index)
-      continue;
-    if(use_index.count(i) == 1) continue;
-    if(abs(b - near_b) <= 10) break;
-    if(b-near_b > abs(b-(near_b+v[i]))+10 && v[i] > 10)
-    {
-      res += 10;
-      near_b += v[i];
-      use_index.insert(i);
-    }
-  }
-  res += abs(b - near_b);
-
-  ///////////
- for (ll i = 0; i < n; i++) {
-    if(i == near_c_index || near_b_index == i ||  i == near_a_index)
-      continue;
-    if(use_index.count(i) == 1) continue;
-    if(abs(a - near_a) <= 10) break;
-    if(a-near_a > abs(a-(near_a+v[i]))+10 && v[i] > 10)
-    {
-      res += 10;
-      near_a += v[i];
-      use_index.insert(i);
-    }
-  }
-  res += abs(a - near_a);
 
   cout << res << endl;
 }
