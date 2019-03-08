4829fd27ccf8bbfcb2fd14bb34d38e4b9edaeef6
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-03-08 19:52:03 +0900

    abc099c

--- cpp/newABC/099/c/main.cpp
+++ cpp/newABC/099/c/main.cpp
@@ -10,7 +10,33 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<int, int> p;
+int n;
+
+// void init(){
+//   vector<int> tmp(100010, INT_MAX);
+//   tmp[0] = 1;
+//   // tmp[1] = 1;
+//   dp = tmp;
+// }
 
 int main(){
+  cin >> n;
+  vector<int> dp(100100, INT_MAX);
+  dp[0] = 0;
 
+  for (int i = 1; i < 100100; i++) {
+    int power = 1;
+    while(power <= i) {
+      dp[i] = min(dp[i], dp[i-power]+1);
+      power *= 6;
+    }
+
+    power = 1;
+    while(power <= i) {
+      dp[i] = min(dp[i], dp[i-power]+1);
+      power *= 9;
+    }
+  }
+
+  cout << dp[n] << endl;
 }
